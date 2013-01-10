aa201820075a304bf69cc90b9b14a4908d579511
shado_000 <veresbogdan91@gmail.com>
2013-01-10 20:39:20 +0200

    space

--- src/main/grammar/Grammar.java
+++ src/main/grammar/Grammar.java
@@ -8,180 +8,141 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-/**
- * Created with IntelliJ IDEA.
- * User: nataliglodeanu
- * Date: 11/12/12
- * Time: 2:23 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Grammar {
     private List<String> terminals;
     private List<String> nonTerminals;
     private List<Production> productions;
     private String startingSymbol;
-    public  Grammar()
-    {
-        this.nonTerminals=new ArrayList<String>();
-        this.terminals=new ArrayList<String>();
-        this.productions=new ArrayList<Production>();
+
+    public  Grammar() {
+        this.nonTerminals = new ArrayList<String>();
+        this.terminals = new ArrayList<String>();
+        this.productions = new ArrayList<Production>();
     }
-    public void readGrammarFromFile()
-    {
-        try{
 
-
-            FileInputStream fstream = new FileInputStream("grammar.txt");
-            DataInputStream in = new DataInputStream(fstream);
+    public void readGrammarFromFile() {
+        try {
+            FileInputStream fStream = new FileInputStream("grammar.txt");
+            DataInputStream in = new DataInputStream(fStream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String strLine;
-            int index=0;
-            while((strLine = br.readLine()) != null)
-            {
-
+            int index = 0;
+            while ((strLine = br.readLine()) != null) {
                 strLine.trim();
-                StringTokenizer stringTokenizer=new StringTokenizer(strLine," ");
-                if(index==0)
-                {
+                StringTokenizer stringTokenizer = new StringTokenizer(strLine," ");
 
-                    while(stringTokenizer.hasMoreElements())
-                    {
+                if (index == 0) {
+                    while (stringTokenizer.hasMoreElements()) {
                         String stringToken =stringTokenizer.nextElement().toString();
-                        if(stringToken!=null)
+                        if(stringToken != null)
                             terminals.add(stringToken );
                     }
-                }
-                else
-                {
-                    if(index==1)
-                    {
-                        while(stringTokenizer.hasMoreTokens())
-                        {
+                } else {
+                    if (index == 1) {
+                        while (stringTokenizer.hasMoreTokens()) {
                             nonTerminals.add(stringTokenizer.nextToken());
                         }
-                    }
-                    else
-                    {
-                        if(index==2)
-                        {
-                            startingSymbol=strLine;
-                        }
-                        else
-                        {
-                            Production production=new Production();
-                            String[] splited=strLine.split("->");
+                    } else {
+                        if (index == 2) {
+                            startingSymbol = strLine;
+                        } else {
+                            Production production = new Production();
+                            String[] splited = strLine.split("->");
                             production.setNonTerminal(splited[0].trim());
                             stringTokenizer=new StringTokenizer(splited[1],"|");
-                            while(stringTokenizer.hasMoreTokens())
-                            {
+                            while(stringTokenizer.hasMoreTokens()) {
                                 production.addResult((stringTokenizer.nextToken().trim()));
                             }
+
                             this.productions.add(production);
                         }
                     }
                 }
                 index++;
             }
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public boolean isGrammarRegular()
-    {
-        for(Production production:productions)
-        {
 
-            for(String result:production.getResults())
-            {
-                if(result.length()>2)
-                {
-
+    public boolean isGrammarRegular() {
+        for(Production production:productions) {
+            for(String result:production.getResults()) {
+                if (result.length() > 2) {
                     return false;
                 }
-                if( this.isTerminal(String.valueOf(result.charAt(0))) )
-                {
-
+                if (this.isTerminal(String.valueOf(result.charAt(0)))) {
                     return false;
                 }
-                if( (result.length()==2 && this.isTerminal(String.valueOf(result.charAt(0)))))
-                {
+                if ((result.length() == 2 && this.isTerminal(String.valueOf(result.charAt(0))))) {
                     return false;
                 }
-                if(String.valueOf(result.charAt(0)).equals("@") && production.getNonTerminal().equals(this.startingSymbol) )
-                {
+                if (String.valueOf(result.charAt(0)).equals("@") && production.getNonTerminal().equals(this.startingSymbol)) {
                     return false;
                 }
             }
         }
-
         return true;
     }
-    public boolean isNonTerminal(String symbol)
-    {
-        for(String nonTerminal:this.nonTerminals)
-        {
-            if(nonTerminal.equals(symbol))
+
+    public boolean isNonTerminal(String symbol) {
+        for (String nonTerminal: this.nonTerminals) {
+            if (nonTerminal.equals(symbol)) {
                 return true;
-        }
-        return false;
-    }
-    public boolean isTerminal(String symbol)
-    {
-        for(String terminal:this.terminals)
-        {
-            if(terminal.equals(symbol))
-                return true;
+            }
         }
         return false;
     }
 
-    public void setStartingSymbol(String startingSymbol)
-    {
-        this.startingSymbol=startingSymbol;
+    public boolean isTerminal(String symbol) {
+        for (String terminal: this.terminals) {
+            if (terminal.equals(symbol)) {
+                return true;
+            }
+        }
+        return false;
     }
-    public void addNonTerminal(String nonTerminal)
-    {
+
+    public void setStartingSymbol(String startingSymbol) {
+        this.startingSymbol = startingSymbol;
+    }
+
+    public void addNonTerminal(String nonTerminal) {
         this.nonTerminals.add(nonTerminal);
     }
-    public void addTerminal(String terminal)
-    {
+
+    public void addTerminal(String terminal) {
         this.terminals.add(terminal);
     }
-    public void addProduction(Production prod)
-    {
+
+    public void addProduction(Production prod) {
         this.productions.add(prod);
     }
-    public void printGrammar() {
 
+    public void printGrammar() {
         System.out.println("\nSet of non-terminals");
-        for(String nonterm:nonTerminals)
-        {
-            System.out.print(nonterm+ " ");
+
+        for (String nonTerm: nonTerminals) {
+            System.out.print(nonTerm + " ");
         }
         System.out.println("\nSet of terminals ");
-        System.out.println("\nStarting symbol "+startingSymbol);
-        for(String term:terminals)
-        {
-            System.out.print(term+ " ");
+        System.out.println("\nStarting symbol " + startingSymbol);
+
+        for (String term: terminals) {
+            System.out.print(term + " ");
         }
         System.out.println("\nSet of productions ");
-        for(Production production:productions)
-        {
+        for (Production production: productions) {
             System.out.println(production.toString() + " ");
         }
     }
-    public Production getProductionFromList(String prod)
-    {
-        for(Production production:productions)
-        {
-            if(production.getNonTerminal().equals(prod))
-            {
+
+    public Production getProductionFromList(String prod) {
+        for(Production production: productions) {
+            if(production.getNonTerminal().equals(prod)) {
                 return production;
             }
         }
         return null;
     }
-
-
 }
--- src/main/grammar/Production.java
+++ src/main/grammar/Production.java
@@ -3,19 +3,12 @@ package main.grammar;
 import java.util.List;
 import java.util.Vector;
 
-/**
- * Created with IntelliJ IDEA.
- * User: nataliglodeanu
- * Date: 11/12/12
- * Time: 2:50 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Production {
     private String nonTerminal;
     private Vector<String> results;
-    public Production()
-    {
-        this.results=new Vector<String>();
+
+    public Production() {
+        this.results = new Vector<String>();
     }
 
     public String getNonTerminal() {
@@ -33,32 +26,27 @@ public class Production {
     public void setResults(Vector<String> results) {
         this.results = results;
     }
-    public void addResult(String result)
-    {
+
+    public void addResult(String result) {
         this.results.add(result);
     }
-    public String toString()
-    {
-        StringBuilder stringBuilder=new StringBuilder();
-        for(String result:results)
-        {
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String result: results) {
             stringBuilder.append(result);
             stringBuilder.append(" | ");
         }
-
-        return this.nonTerminal+"->"+stringBuilder.substring(0,stringBuilder.length()-2);
+        return this.nonTerminal + "->" + stringBuilder.substring(0, stringBuilder.length()-2);
     }
-    public boolean hasResult(String result)
-    {
-        for(String resultFromList:results)
-        {
-            if(resultFromList.equals(result))
-            {
+
+    public boolean hasResult(String result) {
+        for (String resultFromList: results) {
+            if (resultFromList.equals(result)) {
                 return true;
             }
         }
         return false;
     }
-
-
 }
