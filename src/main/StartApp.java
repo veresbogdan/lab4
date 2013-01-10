98127cbd00dd06849db173d7401a47e74ad7efd2
shado_000 <veresbogdan91@gmail.com>
2013-01-10 19:32:28 +0200

    first

---
+++ src/main/StartApp.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shado_000
+ * Date: 10.01.2013
+ * Time: 19:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartApp {
+}
